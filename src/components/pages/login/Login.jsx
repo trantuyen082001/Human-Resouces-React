7c36ede0efd82025c92e524de969b3618fc15d5d
minh duc <69248042+beobiebom@users.noreply.github.com>
2021-12-26 09:08:26 +0700

    delete file constant.js and add api url in .env

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,48 +1,61 @@
-import "./login.css";
-import React, {useState} from 'react';
-import axios  from "axios";
-import {useHistory} from "react-router-dom";
-import { apiUrl } from "../../contain";
+import './login.css'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
-export default function Login({setLoginUser}) {
+export default function Login({ setLoginUser }) {
+  const history = useHistory()
 
-    const history = useHistory();
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  })
 
-    const [ user, setUser] = useState({
-        email: "",
-        password: "",
-    });
+  const handleChange = e => {
+    const { name, value } = e.target
+    setUser({
+      ...user,
+      [name]: value
+    })
+  }
 
-    const handleChange = (e) => {
-        const {name,value} = e.target;
-        setUser({
-            ...user,
-            [name]: value
-        })
-    };
+  const login = () => {
+    console.log(process.env.REACT_APP_API_URL)
 
-    
-    const login = () => {
-        axios.post(`${apiUrl}/login`, user)
-        .then(res => {
-            alert(res.data.message)
-            setLoginUser(res.data.user)
-            history.push("/");
-        })
-    }
+    axios.post(`${process.env.REACT_APP_API_URL}/login`, user).then(res => {
+      alert(res.data.message)
+      setLoginUser(res.data.user)
+      history.push('/')
+    })
+  }
 
-
-    return (
-        <div className="container">
-            <div className="login">
-                {console.log(user)}
-                <h1>Login</h1>
-                <input type="text" name="email" value={user.email} onChange={handleChange} placeholder="Enter your Email"></input>
-                <input type="password" name="password" value={user.password} onChange={handleChange} placeholder="Enter your Password"></input>
-                <div className="button" onClick={login}>Login</div>
-                <div>or</div>
-                <div className="button" onClick={() => history.push("/register")}>Register</div>
-            </div>
+  return (
+    <div className='container'>
+      <div className='login'>
+        {console.log(user)}
+        <h1>Login</h1>
+        <input
+          type='text'
+          name='email'
+          value={user.email}
+          onChange={handleChange}
+          placeholder='Enter your Email'
+        ></input>
+        <input
+          type='password'
+          name='password'
+          value={user.password}
+          onChange={handleChange}
+          placeholder='Enter your Password'
+        ></input>
+        <div className='button' onClick={login}>
+          Login
         </div>
-    )
+        <div>or</div>
+        <div className='button' onClick={() => history.push('/register')}>
+          Register
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/pages/register/register.jsx
+++ src/components/pages/register/register.jsx
@@ -1,71 +1,82 @@
-import "./register.css";
-import React, {useState} from 'react';
-import axios from "axios";
-import { useHistory } from "react-router-dom";
-import validator from 'validator';
-import { apiUrl } from "../../contain";
+import './register.css'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
+import validator from 'validator'
 
 export default function Register() {
+  const history = useHistory()
 
-    const history = useHistory();
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    reEnterPassword: ''
+  })
 
-    const [ user, setUser] = useState({
-        name: "",
-        email: "",
-        password: "",
-        reEnterPassword: "",
-    });
+  const handleChange = e => {
+    const { name, value } = e.target
+    setUser({
+      ...user,
+      [name]: value
+    })
+  }
 
-    const handleChange = (e) => {
-        const {name,value} = e.target;
-        setUser({
-            ...user,
-            [name]: value
+  // const [emailError, setEmailError] = useState('');
+  // const validateEmail = (e) => {
+  //     var email = e.target.value;
+  //     if(validator.isEmail(email)) {
+  //         setEmailError('Valid Email');
+  //     } else {
+  //         setEmailError('Enter valid email');
+  //     }
+  // }
+
+  const register = () => {
+    const { name, email, password, reEnterPassword } = user
+    if (name && email && password && password === reEnterPassword) {
+      if (validator.isEmail(email)) {
+        // alert("Valid email");
+        axios.post(`${process.env.REACT_APP_API_URL}/register`, user).then(res => {
+          alert(res.data.message)
+          history.push('/login')
         })
-    };
-
-    // const [emailError, setEmailError] = useState('');
-    // const validateEmail = (e) => {
-    //     var email = e.target.value;
-    //     if(validator.isEmail(email)) {
-    //         setEmailError('Valid Email');
-    //     } else {
-    //         setEmailError('Enter valid email');
-    //     }
-    // }
-    
-
-    const register = () => {
-        const { name, email, password, reEnterPassword } = user;
-        if( name && email && password && (password === reEnterPassword)){
-            if(validator.isEmail(email)) {
-                // alert("Valid email");
-                axios.post(`${apiUrl}/register`, user)
-                .then( res => {
-                    alert(res.data.message)
-                    history.push("/login")
-                })
-            } else{
-                alert("Enter valid email")
-            }
-        } else {
-            alert("Invlid input")
-        }
-        
+      } else {
+        alert('Enter valid email')
+      }
+    } else {
+      alert('Invlid input')
     }
+  }
 
-    return (
-        <div className="container">
-            <div className="register">
-                <h1>Register</h1>
-                <input type="text" name="name" value={user.name} placeholder="Your Name" onChange={handleChange}></input>
-                <input type="text" name="email" value={user.email} placeholder="Your Email" onChange={handleChange}></input>
-                <input type="password" name="password" value={user.password} placeholder="Enter your Password" onChange={handleChange}></input>
-                <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Re-enter your Password" onChange={handleChange}></input>
-                <div className="button" onClick={register}>Register</div>
-                <div>or</div>
-                <div className="button" onClick={() => history.push("/login")}>Login</div>
-            </div>
+  return (
+    <div className='container'>
+      <div className='register'>
+        <h1>Register</h1>
+        <input type='text' name='name' value={user.name} placeholder='Your Name' onChange={handleChange}></input>
+        <input type='text' name='email' value={user.email} placeholder='Your Email' onChange={handleChange}></input>
+        <input
+          type='password'
+          name='password'
+          value={user.password}
+          placeholder='Enter your Password'
+          onChange={handleChange}
+        ></input>
+        <input
+          type='password'
+          name='reEnterPassword'
+          value={user.reEnterPassword}
+          placeholder='Re-enter your Password'
+          onChange={handleChange}
+        ></input>
+        <div className='button' onClick={register}>
+          Register
         </div>
-    )
+        <div>or</div>
+        <div className='button' onClick={() => history.push('/login')}>
+          Login
+        </div>
+      </div>
+    </div>
+  )
 }
